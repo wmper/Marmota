fd9a692456b2a3870b25a3bfcdcdcb77a3cf5a39
wmper <306632529@qq.com>
2019-11-08 14:37:38 +0800

    抽象类

---
+++ Marmota.Abstractions/IMarmotaBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Marmota.Abstractions
+{
+    public interface IMarmotaBuilder
+    {
+        IServiceCollection Services { get; }
+
+        IConfiguration Configuration { get; }
+
+        IMarmotaBuilder Build();
+    }
+}
