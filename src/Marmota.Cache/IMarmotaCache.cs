b3264bbc90b4220f1be58deb26fd39596b33f0cd
wmper <306632529@qq.com>
2019-12-13 16:46:51 +0800

    新增缓存

---
+++ src/Marmota.Cache/IMarmotaCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marmota.Cache
+{
+    public interface IMarmotaCache
+    {
+        T Get<T>(string key);
+
+        void Set<T>(string key, T value);
+    }
+}
